ef237f95a41d30792e4bbcbbf56368d5a8aaf00c
Cullen S <cullensmi@gmail.com>
2020-11-08 22:38:10 -0500

    updated readme

--- src/com/csmithswim/collections/MapDemo.java
+++ src/com/csmithswim/collections/MapDemo.java
@@ -1,4 +1,15 @@
 package com.csmithswim.collections;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MapDemo {
+    public static void show(){
+        List<Customer> customers = new ArrayList<>();
+        // In comps science we use the big O notation that is the cost of the algorithm
+        //O(n) - O(10)
+        for (var customer : customers)
+            if (customer.getEmail() == "e1")
+                System.out.println("Found!");
+    }
 }
